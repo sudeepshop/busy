97fe9828bc89114b79354d9169bc82587a91409d
sudeepshop <35597206+sudeepshop@users.noreply.github.com>
2018-01-19 16:45:15 +0545

    Update SignUp.js

--- src/client/components/Sidebar/SignUp.js
+++ src/client/components/Sidebar/SignUp.js
@@ -5,7 +5,7 @@ import './SidebarBlock.less';
 const SignUp = () => (
   <div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
-      <FormattedMessage id="new_to_busy" defaultMessage="New to Busy?" />
+      <FormattedMessage id="new_to_busy" defaultMessage="New to Power4Nepal ?" />
     </h3>
     <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
       <button className="SidebarBlock__button">
