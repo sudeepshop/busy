0a10d7479ab95771b773b292a2191ab552b903fd
sudeepshop <35597206+sudeepshop@users.noreply.github.com>
2018-01-19 16:41:40 +0545

    Update Topnav.js

--- src/client/components/Navigation/Topnav.js
+++ src/client/components/Navigation/Topnav.js
@@ -254,7 +254,7 @@ class Topnav extends React.Component {
           <div className={classNames('left', { 'Topnav__mobile-hidden': searchBarActive })}>
             <Link className="Topnav__brand" to="/">
               <i className="iconfont icon-busy Topnav__brand-icon" />
-              busy
+              4Nepal
             </Link>
             <span className="Topnav__version">beta</span>
           </div>
