07132e3e4e77220554038caab05a1c1d7c22c8ca
sudeepshop <35597206+sudeepshop@users.noreply.github.com>
2018-01-20 13:23:39 +0545

    Update webpack-dev-server.js

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -17,7 +17,7 @@ module.exports = {
       'process.env': {
         NODE_ENV: JSON.stringify('development'),
         ENABLE_LOGGER: JSON.stringify(process.env.ENABLE_LOGGER),
-        IMG_HOST: JSON.stringify(process.env.IMG_HOST || 'https://pranishbusy.herokuapp.com'),
+        IMG_HOST: JSON.stringify(process.env.IMG_HOST || 'https://img.busy.org'),
         SENTRY_PUBLIC_DSN: null,
         STEEMCONNECT_CLIENT_ID: JSON.stringify(process.env.STEEMCONNECT_CLIENT_ID || 'power.nepal'),
         STEEMCONNECT_REDIRECT_URL: JSON.stringify(
